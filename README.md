dd4f7cc45ba0434b53f974b3f6ade6c0800b9540
Eleazar Meza <bairesdev_emeza@iseatz.com>
2019-11-21 17:16:17 -0400

    Implemented bubble_sort_by method

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# advanced-building-blocks
+# Advanced building blocks
+The project consits on implementing the bubble sort algorithm in Ruby
+
+## Link to assignment
+
+The assignment can be found [here](https://www.theodinproject.com/courses/ruby-programming/lessons/advanced-building-blocks).
+
+## Technologies used
+
+Just plain ruby.
+
+## Contributors
+
+Eleazar Meza - [GitHub](https://github.com/elshaka)
+Ermin Cahtarevic - [GitHub](https://github.com/ermin-cahtarevic)
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,20 @@
-def bubble_sort(array)
+def bubble_sort_by(array)
   n = array.length
   loop do
     swapped = false
     for i in 1..n-1 do
-      if array[i-1] > array[i] 
+      if yield(array[i-1], array[i]) > 0
         array[i-1], array[i] = array[i], array[i-1]
         swapped = true
-      end 
+      end
     end
     break if !swapped
   end
   return array
-end   
+end
 
-puts bubble_sort([4,3,78,2,0,2])
-      
+def bubble_sort(array)
+  bubble_sort_by(array) { |a, b| a - b }
+end
+
+puts bubble_sort([4,3,78,2,0,2]).to_s
