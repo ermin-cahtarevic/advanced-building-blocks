61b926ffbd2b0b87b3b7540d800e82ec19746088
Ermin Cahtarevic <erminc@windowslive.com>
2019-11-21 22:28:35 +0100

    fixing stickler issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 def bubble_sort_by(array)
   n = array.length
   loop do
     swapped = false
     for i in 1..n-1 do
-      if yield(array[i-1], array[i]) > 0
-        array[i-1], array[i] = array[i], array[i-1]
+      if yield(array[i - 1], array[i]).positive?
+        array[i - 1], array[i] = array[i], array[i - 1]
         swapped = true
       end
     end
-    break if !swapped
+    break unless !swapped
   end
-  return array
+  array
 end
 
 def bubble_sort(array)
   bubble_sort_by(array) { |a, b| a - b }
 end
 
-puts bubble_sort([4,3,78,2,0,2]).to_s
+puts bubble_sort([4, 3, 78, 2, 0, 2]).to_s
