5b36bed6669dc6c1b7abf261a424fa71bcc7d0f8
Ermin Cahtarevic <erminc@windowslive.com>
2019-11-21 22:34:00 +0100

    stickler issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,7 +4,7 @@ def bubble_sort_by(array)
   n = array.length
   loop do
     swapped = false
-    (1..n - 1).each do |i| 
+    (1..n - 1).each do |i|
       if yield(array[i - 1], array[i]).positive?
         array[i - 1], array[i] = array[i], array[i - 1]
         swapped = true
